9ab1d6dcc572edbe9431382f2a0b598b9265ec98
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-06-22 14:37:22 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (15.76%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,8 +1,3 @@
 class Solution(object):
     def heightChecker(self, heights):
-        expected = sorted(heights)
-        n = 0
-        for i in range(len(heights)):
-            if heights[i] != expected[i]:
-                n = n + 1
-        return n
+        return sum(h != e for h, e in zip(heights, sorted(heights)))
