346a5b4276d025451e1886df06dd024260c81fe1
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-08-06 22:42:04 +0300

    Time: 2 ms (83.56%) | Memory: 12.6 MB (42.28%) - LeetSync

--- 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.py
+++ 4271-find-the-degree-of-each-vertex/find-the-degree-of-each-vertex.py
@@ -4,5 +4,4 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        # Sum each row in the adjacency matrix to get the degree of each vertex
         return [sum(row) for row in matrix]
