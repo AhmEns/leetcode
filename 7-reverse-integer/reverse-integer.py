37caf4e4ef05241ca3434b44e76e308cbd66f03c
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-08-07 17:00:17 +0300

    Time: 15 ms (75.23%) | Memory: 12.4 MB (14.20%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -5,19 +5,13 @@ class Solution(object):
         :rtype: int
         """
         neg = x < 0
-        s = list(str(abs(x)))
-        i = 0
-        j = len(s) - 1
-
-        while i < j:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
-                
-        rev = int("".join(s))
+        
+        rev = int(str(abs(x))[::-1])
+        
         if neg:
             rev = -rev
-
+            
         if rev > 2147483647 or rev < -2147483648:
             return 0
+            
         return rev
