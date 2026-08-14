147e9a28f6386e53d933459339f0b0409a715bf6
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-08-14 19:06:45 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (58.76%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -7,16 +7,8 @@ class Solution(object):
         
         for i in image:
             left, right = 0, len(i)- 1
-            while left < right:
-                i[left], i[right] = i[right], i[left]
+            while left <= right:
+                i[left], i[right] = i[right] ^ 1, i[left] ^ 1
                 left += 1
                 right -= 1
-        for i in image:
-            n = 0
-            while n < len(i):
-                if i[n] == 1:
-                    i[n] = 0
-                else:
-                    i[n] = 1
-                n += 1
         return image
