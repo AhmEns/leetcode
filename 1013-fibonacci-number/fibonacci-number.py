9f48f1e52fa5708b07a0d2ba433eb6287bcf5b3f
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-08-17 17:07:10 +0300

    Time: 13 ms (81.18%) | Memory: 12.3 MB (54.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,13 +1,6 @@
 class Solution(object):
     def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        n1, n2, n3 = 0, 1, 0
-        while n > 0:
-            n3 = n1
-            n1 = n1 + n2
-            n2 = n3
-            n -= 1
-        return n1
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a
