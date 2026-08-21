1cc24036798006d5865bfcbb0ecd87f50fbc26ea
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-08-21 22:15:19 +0300

    Time: 4 ms (82.08%) | Memory: 12.6 MB (21.99%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,23 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        dummy = ListNode(0)
-        current = dummy
-        carry = 0 
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
 
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        num1 = 0
+        num2 = 0
+        multiplier = 1
 
-            toplam = val1 + val2 + carry
-            carry = toplam // 10
-            digit = toplam % 10
+        while l1:
+            num1 += l1.val * multiplier
+            multiplier *= 10
+            l1 = l1.next
 
-            current.next = ListNode(digit)
-            current = current.next
+        multiplier = 1
+        while l2:   
+            num2 += l2.val * multiplier
+            multiplier *= 10
+            l2 = l2.next
 
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+        total = num1 + num2
 
-        return dummy.next
+        if total == 0:
+            return ListNode(0)  
+        elif total < 0:
+            return None
+        else:
+            head = None
+            current = None
+
+            while total > 0:
+                digit = total % 10
+                total //= 10
+
+                new_node = ListNode(digit)
+
+                if head is None:
+                    head = new_node
+                    current = new_node
+                else:
+                    current.next = new_node
+                    current = current.next
+
+            return head
