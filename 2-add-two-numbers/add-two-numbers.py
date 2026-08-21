effa403c94da39622e3a8a1a0cbb48593f610513
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-08-21 22:14:44 +0300

    Time: 6 ms (66.96%) | Memory: 12.3 MB (92.05%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,52 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+        # 1. Başı kaybetmemek için boş bir sahte düğüm oluştur
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0  # Elde
 
-        num1 = 0
-        num2 = 0
-        multiplier = 1
+        # 2. Listelerden biri bitene veya elde kalmayana kadar tek döngüde dön
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-        while l1:
-            num1 += l1.val * multiplier
-            multiplier *= 10
-            l1 = l1.next
+            toplam = val1 + val2 + carry
+            carry = toplam // 10          # Onlar basamağı (yeni elde)
+            digit = toplam % 10           # Birler basamağı
 
-        multiplier = 1
-        while l2:   
-            num2 += l2.val * multiplier
-            multiplier *= 10
-            l2 = l2.next
+            # Yeni basamağı zincirin ucuna ekle ve ilerle
+            current.next = ListNode(digit)
+            current = current.next
 
-        total = num1 + num2
+            # Varsa bir sonraki düğüme geç
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
-        if total == 0:
-            return ListNode(0)  
-        elif total < 0:
-            return None
-        else:
-            head = None
-            current = None
-
-            while total > 0:
-                digit = total % 10
-                total //= 10
-
-                new_node = ListNode(digit)
-
-                if head is None:
-                    head = new_node
-                    current = new_node
-                else:
-                    current.next = new_node
-                    current = current.next
-
-            return head
+        # dummy'nin kendisi sahte olduğundan gerçek liste dummy.next ile başlar
+        return dummy.next
