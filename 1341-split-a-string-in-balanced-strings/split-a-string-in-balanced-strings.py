41c6b5e29a3673a128828d6d393de28101c77d38
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-09-07 20:33:02 +0300

    Time: 4 ms (10.89%) | Memory: 12.3 MB (59.22%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return_value, counter = 0, 0
+        for i in s:
+            if i == "R":
+                counter += 1
+            elif i == "L":
+                counter -= 1
+            if counter == 0:
+                return_value += 1
+        return return_value
