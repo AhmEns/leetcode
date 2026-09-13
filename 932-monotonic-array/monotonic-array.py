47d3dfc2e5b215f41e859a6322cf85683e8ed54d
Ahmet Enes Türk <112501118+AhmEns@users.noreply.github.com>
2026-09-13 22:23:11 +0300

    Time: 17 ms (98.46%) | Memory: 20.4 MB (85.98%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        artan = True
+
+        for i in range(len(nums) - 1):
+            if nums[i] < nums[i + 1]:
+                artan = True
+                break
+            elif nums[i] > nums[i + 1]:
+                artan = False
+                break
+
+        if artan == True:    
+            for i in range(len(nums) - 1):
+                if nums[i] > nums[i + 1]:
+                    return False
+            return True
+        else:
+            for i in range(len(nums) - 1):
+                if nums[i] < nums[i + 1]:
+                    return False
+            return True
